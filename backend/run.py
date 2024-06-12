227026ba912c1e3a57b5ca9dad0f8a54b0e92b54
jc <jcsanchez1@users.noreply.github.com>
2024-06-12 10:52:56 -0600

    eliminacion backend

--- backend/run.py
+++
@@ -1,7 +0,0 @@
-from LanavihCV.backend.app import create_app
-
-# Crea una instancia de la aplicación
-app = create_app()
-    # Ejecuta la aplicación en modo de depuración
-if __name__ == "__main__":
-    app.run(debug=True)
